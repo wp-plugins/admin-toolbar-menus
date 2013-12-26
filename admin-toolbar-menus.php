f20c1ed05e9772509b67118fde20e3d15493e840
Benbodhi <Benbodhi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-26 03:13:22 +0000

    created archive for 1.0 in tags
    git-svn-id: https://plugins.svn.wordpress.org/admin-toolbar-menus/tags/1.0@828509 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin-toolbar-menus.php
@@ -0,0 +1,269 @@
+<?php
+/*
+* Plugin Name: 	Admin Toolbar Menus
+* Plugin URI: 	http://gowebben.com
+* Description: 	Seamlessly adds 3 new menu locations to the admin toolbar and removes the WP logo menu.
+* Version: 		1.0
+* Author: 		Benbodhi
+* Author URI: 	http://benbodhi.com
+* Text Domain: 	admin-toolbar-menus
+* Domain Path:	/languages
+* License: 		GPL2
+*/
+/*  Copyright 2013  Benbodhi  (email : wp@benbodhi.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+* internationalization / localization
+*/
+load_plugin_textdomain('admin-toolbar-menus', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+/*
+* register menus
+*/
+function bodhi_atm_reg_admin_menus() {
+	register_nav_menus(
+		array(
+		'bodhi_admin_menu' 		=> __( 'Toolbar Menu (Site Name)', 'admin-toolbar-menus' ),
+		'bodhi_admin_menu2' 	=> __( 'Toolbar Menu (Main)', 'admin-toolbar-menus' ),
+		'bodhi_admin_menu3' 	=> __( 'Toolbar Menu (My Account)', 'admin-toolbar-menus' )
+		)
+	);
+}
+add_action( 'init', 'bodhi_atm_reg_admin_menus' );
+
+/*
+* build out the menus
+*/
+function bodhi_atm_add_admin_menus() {
+
+	global $wp_admin_bar;
+
+	$menu_name 	= 'bodhi_admin_menu';
+	$menu_name2 = 'bodhi_admin_menu2';
+	$menu_name3 = 'bodhi_admin_menu3';
+
+	// start menu1 - site-name menu
+	if( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+
+		$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+		$menu_items = wp_get_nav_menu_items( $menu->term_id );
+
+		foreach( (array) $menu_items as $key => $menu_item ) {
+
+			if( $menu_item->classes ) {
+
+				$classes = implode( ' ', $menu_item->classes );
+
+			} else {
+
+				$classes = "";
+
+			}
+
+			$meta = array(
+				'class' 	=> $classes,
+				'onclick' 	=> '',
+				'target' 	=> $menu_item->target,
+				'title' 	=> $menu_item->attr_title
+			);
+
+			if( $menu_item->menu_item_parent ) {
+
+				$wp_admin_bar->add_menu(
+					array(
+					'id' 		=> $menu_item->ID,
+					'parent' 	=> $menu_item->menu_item_parent, 
+					'title' 	=> $menu_item->title,
+					'href' 		=> $menu_item->url,
+					'meta' 		=> $meta
+					)
+				);
+
+			} else {
+
+				$wp_admin_bar->add_menu(
+					array(
+					'id' 		=> $menu_item->ID,
+					'parent' 	=> 'site-name',
+					'title' 	=> $menu_item->title,
+					'href' 		=> $menu_item->url,
+					'meta' 		=> $meta
+					)
+				);
+			}
+		} // end foreach
+	} // end menu1 - site-name menu
+
+	// start menu2 - toolbar menu
+	if( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name2 ] ) ) {
+
+		$menu2 = wp_get_nav_menu_object( $locations[ $menu_name2 ] );
+		$menu_items = wp_get_nav_menu_items( $menu2->term_id );
+
+		foreach( (array) $menu_items as $key => $menu_item ) {
+
+			if( $menu_item->classes ) {
+
+				$classes = implode( ' ', $menu_item->classes );
+
+			} else {
+
+				$classes = "";
+
+			}
+
+			$meta = array(
+				'class' 	=> $classes,
+				'onclick' 	=> '',
+				'target' 	=> $menu_item->target,
+				'title' 	=> $menu_item->attr_title
+			);
+
+			if( $menu_item->menu_item_parent ) {
+
+				$wp_admin_bar->add_menu(
+					array(
+					'id' 		=> $menu_item->ID,
+					'parent' 	=> $menu_item->menu_item_parent, 
+					'title' 	=> $menu_item->title,
+					'href' 		=> $menu_item->url,
+					'meta' 		=> $meta
+					)
+				);
+
+			} else {
+
+				$wp_admin_bar->add_menu(
+					array(
+					'id' 		=> $menu_item->ID,
+					'title' 	=> $menu_item->title,
+					'href' 		=> $menu_item->url,
+					'meta' 		=> $meta
+					)
+				);
+			}
+		} // end foreach
+	} // start menu2 - toolbar menu
+
+	// start menu3 - my-account menu
+	if( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name3 ] ) ) {
+
+		$menu3 = wp_get_nav_menu_object( $locations[ $menu_name3 ] );
+		$menu_items = wp_get_nav_menu_items( $menu3->term_id );
+
+		foreach( (array) $menu_items as $key => $menu_item ) {
+
+			if( $menu_item->classes ) {
+
+				$classes = implode( ' ', $menu_item->classes );
+
+			} else {
+
+				$classes = "";
+
+			}
+
+			$meta = array(
+				'class' 	=> $classes,
+				'onclick' 	=> '',
+				'target' 	=> $menu_item->target,
+				'title' 	=> $menu_item->attr_title
+			);
+
+			if( $menu_item->menu_item_parent ) {
+
+				$wp_admin_bar->add_menu(
+					array(
+					'id' 		=> $menu_item->ID,
+					'parent' 	=> $menu_item->menu_item_parent, 
+					'title' 	=> $menu_item->title,
+					'href' 		=> $menu_item->url,
+					'meta' 		=> $meta
+					)
+				);
+
+			} else {
+
+				$wp_admin_bar->add_menu(
+					array(
+					'id' 		=> $menu_item->ID,
+					'parent'	=> 'my-account',
+					'title' 	=> $menu_item->title,
+					'href' 		=> $menu_item->url,
+					'meta' 		=> $meta
+					)
+				);
+			}
+		} // end foreach
+	} // start menu3 - my-account menu
+}
+add_action( 'admin_bar_menu', 'bodhi_atm_add_admin_menus', 35 );
+
+/*
+* remove existing menu items
+*/
+function bodhi_atm_remove_admin_menus() {
+
+	global $wp_admin_bar;
+
+	if( current_user_can( 'administrator' ) ) { // remove for administrators
+
+		// links to remove existing items from the toolbar
+		$wp_admin_bar->remove_menu('wp-logo'); 			// remove WordPress logo menu
+		// $wp_admin_bar->remove_menu('about');			// WordPress about menu
+	    // $wp_admin_bar->remove_menu('wporg');			// WordPress website
+	    // $wp_admin_bar->remove_menu('documentation');	// WordPress documentation
+	    // $wp_admin_bar->remove_menu('support-forums');// WordPress support forums
+		// $wp_admin_bar->remove_menu('feedback');		// WordPress feedback
+	    // $wp_admin_bar->remove_menu('view-site');		// View current site link
+		// $wp_admin_bar->remove_menu('updates');		// Updates button
+		// $wp_admin_bar->remove_menu('my-account');	// Links to your account. The ID depends upon if you have avatar enabled or not.
+		// $wp_admin_bar->remove_menu('site-name');		// Site name with other dashboard items
+		// $wp_admin_bar->remove_menu('my-sites');		// My Sites menu, if you have more than one site
+		// $wp_admin_bar->remove_menu('get-shortlink');	// Shortlink to a page/post
+		// $wp_admin_bar->remove_menu('edit');			// Post/Page/Category/Tag edit link
+		// $wp_admin_bar->remove_menu('new-content');	// Add New menu
+		// $wp_admin_bar->remove_menu('comments');		// Comments link
+		// $wp_admin_bar->remove_menu('updates');		// Updates link
+		// $wp_admin_bar->remove_menu('search');		// Search box
+
+	} else { // remove for all user roles except administrator
+
+		// links to remove existing items from the toolbar
+		$wp_admin_bar->remove_menu('wp-logo'); 			// remove WordPress logo menu
+		// $wp_admin_bar->remove_menu('about');			// WordPress about menu
+	    // $wp_admin_bar->remove_menu('wporg');			// WordPress website
+	    // $wp_admin_bar->remove_menu('documentation');	// WordPress documentation
+	    // $wp_admin_bar->remove_menu('support-forums');// WordPress support forums
+		// $wp_admin_bar->remove_menu('feedback');		// WordPress feedback
+	    // $wp_admin_bar->remove_menu('view-site');		// View current site link
+		// $wp_admin_bar->remove_menu('updates');		// Updates button
+		// $wp_admin_bar->remove_menu('my-account');	// Links to your account. The ID depends upon if you have avatar enabled or not.
+		// $wp_admin_bar->remove_menu('site-name');		// Site name with other dashboard items
+		// $wp_admin_bar->remove_menu('my-sites');		// My Sites menu, if you have more than one site
+		// $wp_admin_bar->remove_menu('get-shortlink');	// Shortlink to a page/post
+		// $wp_admin_bar->remove_menu('edit');			// Post/Page/Category/Tag edit link
+		// $wp_admin_bar->remove_menu('new-content');	// Add New menu
+		// $wp_admin_bar->remove_menu('comments');		// Comments link
+		// $wp_admin_bar->remove_menu('updates');		// Updates link
+		// $wp_admin_bar->remove_menu('search');		// Search box
+
+	}
+}
+add_action( 'wp_before_admin_bar_render', 'bodhi_atm_remove_admin_menus' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,113 @@
+=== Admin Toolbar Menus ===
+Contributors: Benbodhi
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=F7W2NUFAVQGW2
+Tags: toolbar, admin, menu, menus
+Requires at least: 3.4
+Tested up to: 3.8
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Seamlessly adds 3 new menu locations to the admin toolbar and removes the WP logo menu.
+
+== Description ==
+
+Add 3 new menu locations to the WordPress toolbar so you can quickly and easily create your own custom toolbar menus using the built in WordPress menus page.<br />
+Supports multi level and works seamlessly with the existing toolbar.<br /><br />
+First menu location is under the 'Site Name' menu dropdown on the left side.<br />
+Second menu location is along the main toolbar.<br />
+Third menu location is under the 'Howdy / My Account' dropdown.<br /><br />
+Also removes the WP Logo menu from the top left of the toolbar.<br /><br />
+Super simple to use, just like building any other menu in WordPress using the menus page.<br />
+No settings, just 'plug and play'.
+
+Some of the features included:
+
+*   Simple toolbar menu customization using WordPress menus
+*   Removes WP logo menu from the top left of the admin toolbar
+*   Seamless integration with the existing WordPress menus
+*   No Settings, just 'plug and play'
+
+= Translations =
+
+* English - default, always included
+* *Your translation? - [Just send it in](http://gowebben.com/contact/)*
+
+*Note:* This plugin is localized/translateable by default. This is very important for all users worldwide. So please contribute your language to the plugin to make it even more useful.<br />
+For translating I recommend the awesome ["Codestyling Localization" plugin](http://wordpress.org/extend/plugins/codestyling-localization/) and for validating the ["Poedit Editor"](http://www.poedit.net/).
+
+= Feedback =
+
+* I am open to your suggestions and feedback - Thanks for checking out this plugin!
+* Drop me a line [@benbodhi](http://twitter.com/benbodhi) or [@GoWebben](http://twitter.com/gowebben) on Twitter
+* Follow me on [my Facebook page](http://www.facebook.com/gowebben)
+* Or circle [+GoWebben](http://googleplus.url) on Google Plus ;-)
+
+*Note:* This is the first plugin I have submitted to the WordPress repository, so I hope you like it. Please rate it and feel free to send me any suggestions.
+
+== Installation ==
+
+To install this plugin:
+
+1. Upload folder `admin-toolbar-menus` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Visit the menus page under Appearance > Menus to create your new menus
+
+Or
+
+1. Upload the compressed version `admin-toolbar-menus.zip` through 'Plugins' > 'Add New' > 'Upload'
+2. Click 'Activate Plugin'
+3. Visit the menus page under Appearance > Menus to create your new menus
+
+== Frequently Asked Questions ==
+
+= Does this display menus to admin only or everyone? =
+
+The menus created will be visible to anyone logged in.
+
+= Can you make it show only to admins? =
+
+You could quite easily but you would need to know a little php. You simply need to add a conditional statement to the plugin to make sure only the admin user role can see the menus.
+
+= Do you have to use all 3 menus? =
+
+Nope! If you don't assign a menu to the locations, they simply won't show up.
+
+== Screenshots ==
+
+1. 3 new toolbar menu locations added to your existing locations.
+2. You can see the new locations below when creating your menus.
+3. Example main toolbar menu with multi level flyout.
+4. Example menu under the 'My Account' section. Great for membership sites!
+5. Example menu under the 'My Site' menu, viewed from backend.
+6. Example menu under the 'My Site' menu, viewed from frontend.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial Release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+Initial Release.
+
+== Translations ==
+
+* English - default, always included
+* *Your translation? - [Just send it in](mailto:wp@benbodhi.com)*
+
+*Note:* This plugin is localized/translateable by default. This is very important for all users worldwide. So please contribute your language to the plugin to make it even more useful. For translating I recommend the awesome ["Codestyling Localization" plugin](http://wordpress.org/extend/plugins/codestyling-localization/) and for validating the ["Poedit Editor"](http://www.poedit.net/).
+
+== Additional Info ==
+**Idea Behind / Philosophy:** This is something that I have found super useful when it comes to customizing WordPress for a client. I have been writing custom menus for quite a while and realised there is not much out there like this. No more code snippets, copy pasting or including files, simply install the plugin and away you go using the built in WordPress menus. A simple but powerful plugin to give you more customization powers over the admin toolbar.
+
+== Future Features ==
+* More power over menu locations with options to pick which locations you want to use and the menu order in relation to existing WordPress menus.
+* Options page to remove other standard toolbar menu items. *(you can uncomment these in the code now if you're keen ;)*
+Again, feel free to [shoot me suggestions](mailto:wp@benbodhi.com)
+
+== Credits ==
+[Benbodhi Mantra](http://benbodhi.com/) [@benbodhi](http://twitter.com/benbodhi) from [GoWebben](http://gowebben.com/) [@GoWebben](http://twitter.com/gowebben)
